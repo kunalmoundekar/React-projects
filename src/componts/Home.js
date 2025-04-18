4d37cf1cbf24e5fa831a42a964464fe0efe5f51d
kunalmoundekar <kunalmoundekar94@gmail.com>
2025-04-18 13:17:49 +0530

    new changes

---
+++ src/componts/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Table from './Table'
+import Navbar from './Navbar'
+
+const Home = () => {
+  return (
+    <div>
+        <Navbar/>
+        <Table/>
+      
+    </div>
+  )
+}
+
+export default Home
---
+++ src/componts/Table.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './table.css';
+
+
+const handleClick = () => {
+  document.getElementById('numberInput').value=''
+  document.getElementById('table-output').innerHTML = '';
+}
+
+const handleChange = () => {
+  const inputValue = document.getElementById('numberInput').value;
+  const outputDiv = document.getElementById('table-output'); 
+  outputDiv.innerHTML = ''; 
+
+  for (let i = 1; i <= 10; i++) {
+    const line = document.createElement('p'); 
+    line.textContent = `${inputValue} x ${i} =    ${inputValue * i}`;  
+    outputDiv.appendChild(line); 
+  }
+}
+
+function Table() {
+  return (
+    <div className="container">
+      <div className="description">
+        <p>
+          Enter a number to instantly see its multiplication table—quick, easy, and perfect for learning or teaching!
+        </p>
+      </div>
+
+      <div className="input-section">
+        <input id="numberInput" type="number" placeholder="Enter a number"    onChange={handleChange} />
+        <button  onClick={handleClick} >Clear</button>
+      </div>
+
+      <div className="table-output" id='table-output'>
+      </div>
+    </div>
+  );
+}
+
+export default Table;
