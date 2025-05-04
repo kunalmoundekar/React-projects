8db1efa9a6c3c3fd5ab0d7ac678798e5dfbc8bae
kunalmoundekar <kunalmoundekar94@gmail.com>
2025-05-04 21:26:21 +0530

    Add validation on Login button.

--- ReactProject/LoginFrom/src/AuthFrom.js
+++ ReactProject/LoginFrom/src/AuthFrom.js
@@ -1,44 +1,96 @@
-import React, { useState } from 'react'
+
+import React, { useState } from 'react';
 
 const AuthFrom = () => {
-    const [isLogin, setIsLogin] = useState("")
-    
+    const [isLogin, setIsLogin] = useState(true);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const handleLogin = () => {
+        setError('');
+
+        if (!email || !password) return setError("Email And Password Is Both Are Required")
+
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) return setError("Enter valid Email ");
+
+        if (password.length < 6) return setError("Password must be at least 6 characters long!");
+
+        alert("Login Success ✅");
+        setEmail('');
+        setPassword('');
+
+    };
+
+    const handleSingUp = () => {
+        setError('');
+
+        if (!email || !password) return setError("Email And Password Is Both Are Required")
+
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) return setError("Enter valid Email ");
+
+        if (password.length < 6) return setError("Password must be at least 6 characters long!");
+
+        if (password)
+
+            alert("You’ve Signed Up Successfully! 🎉");
+        setEmail('');
+        setPassword('');
+
+    };
+
     return (
         <div className='container'>
             <div className='form-container'>
                 <div className='form-toggle '>
-                    <button className={isLogin ? 'active' : ""} onClick={() => { setIsLogin(true) }} >Login</button>
-                    <button className={!isLogin ? 'active' : ""} onClick={() => { setIsLogin(false) }}>SignUp</button>
+                    <button className={isLogin ? 'active' : ""} onClick={() => {
+                        setIsLogin(true)
+                    }} >Login</button>
+                    <button className={!isLogin ? 'active' : ""} onClick={() => setIsLogin(false)}>SignUp</button>
                 </div>
-                {
-                    isLogin ? <>
-                        <div className='form'>
-                            <h2>Login From</h2>
-                            <input type='email' placeholder='Email' />
-                            <input type='password' placeholder='password' />
-                            <a href='#'>Forgot password </a>
-                            <button  >Login</button>
-                            <p>
-                                Not a Member ?
-                                <a href='#' onClick={() => setIsLogin(false)} > SignUp Now </a>
-                            </p>
-                        </div>
-                    </> : <>
-                        <div className='form'>
-                            <h2>SignUp From</h2>
-                            <input type='email' placeholder='Email' />
-                            <input type='password' placeholder='password' />
-                            <input type='password' placeholder='Confrim password' />
-                            <button  >SignUp</button>
 
-                        </div>
+                {isLogin ? (
+                    <div className='form'>
+                        <h2>Login Form</h2>
+                        <input
 
+                            type='email'
+                            placeholder='Email'
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <input
+                            type='password'
+                            placeholder='Password'
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        {error && <p >{error}</p>}
+                        <a href='#'>Forgot password</a>
+                        <button onClick={handleLogin}>Login</button>
+                        <p>
+                            Not a Member ?
+                            <a href='#' onClick={() => setIsLogin(false)}> SignUp Now </a>
+                        </p>
+                    </div>
+                ) : (
+                    <div className='form'>
+                        <h2>SignUp Form</h2>
 
-                    </>
-                }
+                        <input type='email' placeholder='Email' onChange={(e) => setEmail(e.target.value)} />
+                        <input type='password' placeholder='Password' onChange={(e) => setPassword(e.target.value)} />
+                        <input type='password' placeholder='Confirm Password' onChange={(e) => setPassword(e.target.value)} />
+                        {error && <p >{error}</p>}
+                        <button onClick={handleSingUp}>SignUp</button>
+                    </div>
+                )}
             </div>
-        </div >
-    )
-}
+        </div>
+    );
+};
 
-export default AuthFrom
+export default AuthFrom;
+
+
