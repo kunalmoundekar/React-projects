0aa6d7747c939c98fbfe3b2fc1e62c4f54f8f886
kunalmoundekar <kunalmoundekar94@gmail.com>
2025-05-04 22:10:34 +0530

    Add validation on SingUp button.

--- ReactProject/LoginFrom/src/AuthFrom.js
+++ ReactProject/LoginFrom/src/AuthFrom.js
@@ -5,6 +5,7 @@ const AuthFrom = () => {
     const [isLogin, setIsLogin] = useState(true);
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
     const [error, setError] = useState('');
 
     const handleLogin = () => {
@@ -26,19 +27,19 @@ const AuthFrom = () => {
     const handleSingUp = () => {
         setError('');
 
-        if (!email || !password) return setError("Email And Password Is Both Are Required")
-
+        if (!email || !password || !confirmPassword) return setError(" All Filed Are Required")
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         if (!emailRegex.test(email)) return setError("Enter valid Email ");
-
         if (password.length < 6) return setError("Password must be at least 6 characters long!");
-
-        if (password)
-
+        if (password !== confirmPassword) {
+            return setError("Passwords do not match!");
+          }
+          else{
             alert("You’ve Signed Up Successfully! 🎉");
-        setEmail('');
-        setPassword('');
-
+          }
+          setEmail('');
+          setPassword('');
+          setConfirmPassword('');
     };
 
     return (
@@ -79,9 +80,9 @@ const AuthFrom = () => {
                     <div className='form'>
                         <h2>SignUp Form</h2>
 
-                        <input type='email' placeholder='Email' onChange={(e) => setEmail(e.target.value)} />
-                        <input type='password' placeholder='Password' onChange={(e) => setPassword(e.target.value)} />
-                        <input type='password' placeholder='Confirm Password' onChange={(e) => setPassword(e.target.value)} />
+                        <input type='email' placeholder='Email'  value={email}  onChange={(e) => setEmail(e.target.value)} />
+                        <input type='password' placeholder='Password'  value={password}  onChange={(e) => setPassword(e.target.value)} />
+                        <input type='password' placeholder='Confirm Password'  value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
                         {error && <p >{error}</p>}
                         <button onClick={handleSingUp}>SignUp</button>
                     </div>
